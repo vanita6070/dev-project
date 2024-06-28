5631407ce04f579e6dd16bb76819a9738fbc6cbd
Rajkumar k <kanche.rajkumar@sakhatech.com>
2024-06-28 22:11:28 +0530

    Dev Application , added index.html

---
+++ src/main/java/vanitaroy/demo/dev_test/DevTestApplication.java
@@ -0,0 +1,13 @@
+package vanitaroy.demo.dev_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vanitaroy/demo/dev_test/DevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package vanitaroy.demo.dev_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
